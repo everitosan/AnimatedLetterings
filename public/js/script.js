067eda56116693c88b8e00481391b577d004cc0e
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-06-22 18:36:13 -0500

    feat: server and files of the site

---
+++ public/js/script.js
@@ -0,0 +1,63 @@
+
+  'use sctrict';
+
+  var s ,lastData, lettering;
+
+  function animateLetters() {
+
+	  s = Snap('1000','1000');
+
+	  Snap.load("../svg/NiLasMoscas.svg",loadSnapCallback);
+
+  }
+
+
+  function loadSnapCallback(data) {
+  		if (data) {
+  			lastData = data;
+	    	lettering = lastData.select("g");
+	  		s.append(lettering);
+	  		lettering.drag();
+  		}
+
+
+	  	var parts = lettering.selectAll(".bigLetters");
+	  	var whiteLetters = lettering.selectAll('.whiteLetters');
+
+	  	var propertiesParts = {	
+	  		'stroke-width': 4.9535,
+	  		'stroke': '#7C5A35',
+	  		'stroke-dashoffset': 0
+	  	};
+
+	  	var propertiesWhiteLetters = {
+	  		'fill-opacity': 1,
+	  	};
+
+	  	parts.attr({'stroke':'#966A40', 'stroke-width': 10, 'fill': '#FFFFFF'});
+	  	whiteLetters.attr({'fill-opacity': 0});
+
+	  	parts.forEach(function(path){
+	  		tLenght = path.getTotalLength();
+
+	  		var strokeProps = {
+	  			'stroke-dasharray': tLenght,
+	  			'stroke-dashoffset': tLenght
+	  		};
+
+	  		path.attr(strokeProps);
+	  	});
+
+
+
+	  	parts.animate(propertiesParts,5000, mina.easeinout);
+	  	whiteLetters.animate(propertiesWhiteLetters,8000, mina.easeinout);
+
+	  	parts.animate({'fill-opacity': 0},8000, mina.easeinout);
+  	
+  }
+
+  document.onload=setTimeout(animateLetters,2500);
+
+
+
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express"),
+    path = require("path"),
+    app = express(),
+    livereload = require('express-livereload');
+
+var server = require('http').createServer(app);
+
+app.set('views', './public');
+app.set('view engine', 'jade');
+
+app.use(express.static (__dirname+'/public'));
+
+
+app.get("/", function(req, res){
+  res.render('index');
+});
+
+livereload(app, config={});
+config.watchDir = __dirname + "/public";
+
+server.listen(process.env.PORT || 8080);
+
